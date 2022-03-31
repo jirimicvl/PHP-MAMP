4fd469565bb7c0df4085fc94b8eb475bb73a580a
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-03-31 12:36:27 +0300

    commit

--- phpDir/src/PHP_practice05/section_b/c05/array-updating-functions.php
+++ phpDir/src/PHP_practice05/section_b/c05/array-updating-functions.php
@@ -4,15 +4,24 @@
 // remove the last item from array, 
 // convert to string and loop through keys and values of the associative array. 
 
-$orderedItems = ["", ];
+$orderedItems = ["flour", "choco", "cola", "laptop"];
 
-
+array_unshift($orderedItems, 'MacBook');
+array_pop($orderedItems);
 
 ?>
 <?php include 'includes/header.php'; ?>
 
 <h1>Order</h1>
+<?php
 
+foreach($orderedItems as $key => $value)
+{
+  echo $key . $value . "<br>";
+}
+
+
+?>
 <!-- // Write your code here -->
 
 <?php include 'includes/footer.php'; ?>
