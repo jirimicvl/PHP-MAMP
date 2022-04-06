a9e024db64073c258c80220489241206f8b279ce
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-04-06 15:00:01 +0300

    commit

--- phpDir/src/PHP_practice06/section_b/c06/oop/section_a/c04/constructor-methods.php
+++ phpDir/src/PHP_practice06/section_b/c06/oop/section_a/c04/constructor-methods.php
@@ -10,36 +10,42 @@ class Account {
   public $balance;
 
     // Step 3: Create a PHP constructor with argument types e.g. number, type and balance If a balance is not given when creating the object, a default value of 0.00 should be used.
-  function __construct($number, $type, $balance = 0.00) {
+  function __construct(int $number, string $type, float $balance = 0.00) {
     $this->number = $number;
     $this->type = $type;
     $this->balance = $balance;    
   }
 
+  function get_number($number) {
+    return $this->number = $number;
+  }
+
+  function set_type($type) {
+    return $this->type = $type;
+  }
+
+  function set_balance($balance) {
+    return $this->balance = $balance;
+  }
+
   // Step 4: Create two methods for deposit and withdraw which will update the value stored in the balance property. They should be given arguments and return type declaration of float.
 
-  function deposit ($balance, $number) {
+  function deposit ($balance, $number): float {
     return $balance += $number;
   }
 
-  function withdraw ($balance, $number) {
+  function withdraw ($balance, $number): float  {
     return $balance -= $number;
-
   }
 
   // Step 5: Create two objects to represent a checking account and savings account. 
 
-
-
 }
 
-
-
-
-
-
-
-
+$savings = new Account();
+$savings->set_type('savings');
+$checkings = new Account();
+$checkings->set_type('checkings');
 
 // Step 6:  An HTML skeleton is drawn. The first row should show headings using  the type property of two objects. To access property use the:
 //  - Name of the variable that holds the object
