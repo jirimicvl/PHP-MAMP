f4a18f20e688f808d04d4b3180787abcd8f33861
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-04-07 11:03:23 +0300

    commit

--- phpDir/src/PHP_practice01/blank.php
+++ phpDir/src/PHP_practice01/blank.php
@@ -1,14 +1,16 @@
+<form action="blank.php" method="post">
+  <label for="username">Username</label>
+  <input type="text" name="username">
+  <label for="password">Password</label>
+  <input type="password" name="password">
+  <input type="submit" name="submit">
+</form>
+
 <?php
 	if (isset($_POST['submit'])) {
 	//   echo "yes, data";
-	  $user = $_POST['username'];
-	  $pass = $_POST['password'];
-
-      if ($user && $pass) {
-	    echo $user . " " . $pass;
-	  } else {
-	    echo "Username and Password field cannot be blank";
-	  }
+	  $user = $_POST['username'];// Get data from username in HTML
+	  $pass = $_POST['password']; // Get data from password in HTML
 
       $host = "db"; //servername normally localhost
       $dbname = "loginapp"; // database name
@@ -23,14 +25,26 @@
       } else {
           echo "Connection ok";
       }
+      
+      if ($user && $pass) {
+	    echo $user . " " . $pass;
+	  } else {
+	    echo "Username and Password field cannot be blank <br/>";
+	  }
+
+      $query = "INSERT INTO Users(username, password)";
+      $query .= "VALUES ('$user'),('$pass')";
+
+      $result = mysqli_query($connection, $query);
+
+      if (!$result) {
+          die('Query failed');
+        } else {
+            echo "QUery success";
+        }
+
+      
 	}
 	
 ?>
 
-<form action="blank.php" method="post">
-  <label for="username">Username</label>
-  <input type="text" name="username">
-  <label for="password">Password</label>
-  <input type="password" name="password">
-  <input type="submit" name="submit">
-</form>
