0e5d7f801768e9109e4243a8e4f1071f9132f89a
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-04-07 13:05:03 +0300

    commit

--- phpDir/src/PHP_practice01/blank.php
+++ phpDir/src/PHP_practice01/blank.php
@@ -1,41 +1,67 @@
-<form action="blank.php" method="post">
+<!-- <form action="blank.php" method="post">
   <label for="username">Username</label>
   <input type="text" name="username">
   <label for="password">Password</label>
   <input type="password" name="password">
   <input type="submit" name="submit">
+</form> -->
+
+<form action="blank.php" method="post">
+  <label for="username">Username</label>
+  <input type="text" name="username">
+  <label for="password">Password</label>
+  <input type="password" name="password">
+  <select name="id">
+      <?php
+      while($row = mysqli_fetch_assoc($result))
+        $id = $row['id'];
+        echo "<option value='$id'>$id</option>"
+      ?>
+
+      <option value="">1</option>
+
+  </select>
+
+  <input type="submit" name="submit" value="UPDATE">
 </form>
 
+<?php include 'db.php'?>
+
 <?php
-	if (isset($_POST['submit'])) {
-	//   echo "yes, data";
-	  $user = $_POST['username'];// Get data from username in HTML
-	  $pass = $_POST['password']; // Get data from password in HTML
+	
 
-      $host = "db"; //servername normally localhost
-      $dbname = "loginapp"; // database name
-      $dbuser = "root"; //  username
-      $dbpass= "lionPass"; //userpassword
+    //   $query = "INSERT INTO Users(username, password)";
+    //   $query .= "VALUES ('$user','$pass')";
 
-      // Step 1) PDO 2) MYSQLI
-      $connection = new mysqli($host, $dbuser, $dbpass, $dbname);
-
-	  if($connection->connect_error) {
-        die("Connection failed" . $connection->error);
-      } else {
-          echo "Connection ok";
-      }
-
-      $query = "INSERT INTO Users(username, password)";
-      $query .= "VALUES ('$user','$pass')";
+    $query = "SELECT * FROM Users";
 
       $result = mysqli_query($connection, $query);
 
       if (!$result) {
           die('Query failed');
-        } 
-
-	}
-	
+        }
 ?>
+
+<?php
+        if (isset($_POST['submit'])) {
+            $user = $_POST['username'];// Get data from username in HTML
+            $pass = $_POST['password']; // Get data from password in HTML
+            $id     = $_POST['id'];
+          }
+
+          $query = "UPDATE Users SET ";
+          $query .= "username = '$user',"; 
+          $query .= "password = '$pass'"; 
+          $query .= "WHERE ID = $id";
+
+          $result = mysqli_query($connection, $query);
+
+        if (!$result) {
+          die('Update Query failed');
+        }
+?>
+
+
+
+
 
---
+++ phpDir/src/PHP_practice01/db.php
@@ -0,0 +1,20 @@
+<?php
+      $host = "db"; //servername normally localhost
+      $dbname = "loginapp"; // database name
+      $dbuser = "root"; //  username
+      $dbpass= "lionPass"; //userpassword
+
+      // Step 1) PDO 2) MYSQLI
+      $connection = new mysqli($host, $dbuser, $dbpass, $dbname);
+
+	  if($connection->connect_error) {
+        die("Connection failed" . $connection->connect_error);
+      } 
+      else {
+          echo "Connection ok";
+      }
+
+      
+
+
+?>
