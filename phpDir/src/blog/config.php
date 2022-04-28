4384c17dda9a73211eb4a4e38c46c19d321d23b2
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-04-28 21:06:26 +0300

    commit

---
+++ phpDir/src/blog/config.php
@@ -0,0 +1,12 @@
+<?php
+
+    $host = 'db';
+    $dbname = 'todoDB';
+    $dbuser = 'root';
+    $dbpass = 'lionPass';
+
+    // Check the MySQL connection status
+    $conn = new mysqli($host, $dbuser, $dbpass, $dbname);
+
+
+?>
